91acd585f4c061d0616d91dc9cadef714c79a8ef
toul <toul@users.noreply.github.com>
2015-06-20 14:20:21 -0400

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -7,7 +7,6 @@
 ##      4.get the inverse of a square matrix
 
 makeCacheMatrix <- function(x = matrix()) {
-
     # When makeCacheMatrix is called, 'x' contains the original matrix and its 
     # inverse matrix 'Inv_m' is set to NULL (it has not yet been calculated).
     Inv_m <- NULL
@@ -17,7 +16,6 @@ makeCacheMatrix <- function(x = matrix()) {
         # Since 'x' and 'Inv_m' were declared in the parent environment of this function, you
         # must use the <<- assignment to correctly access them. 
         
-        message("re-assigning the original matrix and clearning its cached inverse")
         x <<- y
         Inv_m <<- NULL
     }
@@ -25,20 +23,17 @@ makeCacheMatrix <- function(x = matrix()) {
         # Return the original matrix.
         # Since 'x' was not defined in this function, R searches its parent environment and finds 'x'there.
         
-        message("returning original matrix")
         x
     }
     setInv_m <- function(i){
         # Cache the inverse matrix using the values from 'i'.
         # The '<<-' assignment operator ensures 'Inv_m', declared in the parent environment, is assigned.
         
-        message("setting inverse matrix cache")  
         Inv_m <<- i
     }
     getInv_m <- function() {
         # Return the cached inverse matrix, whether it is NULL or not.
         
-        #message("returning the cached inverse matrix - NULL or !NULL")  
         Inv_m
     }
   
@@ -64,18 +59,64 @@ cacheSolve <- function(x = matrix(), ...) {
     
     # Return the cached inverse matrix if it is not NULL
     if(!is.null(i_m)) {
-        message("returning cached (non-null) inverse matrix")
-        
         return(i_m)
     }
     
     # The cached inverse matrix is NULL, therefore retrieve the original matrix,
     # calculate, cache and return its value.
     
-    message("retrieving original matrix; calculating, caching and returning its inverse matrix")
-    
     data <- x$get_m()
     i_m <- solve(data, ...)
     x$setInv_m(i_m)
     i_m
 }
+
+
+
+
+
+## CODE FOR TESTING IS BELOW:
+
+# Set the original matrix and return the special list of functions
+m_list <- makeCacheMatrix(matrix(c(2,7,-5,3), nrow=2, ncol=2))
+
+# Return original matrix
+m_list$get_m()
+
+# Return the calculated inverse matrix
+cacheSolve(m_list)
+
+# Return the cached inverse matrix
+cacheSolve(m_list)
+
+# Return the inverse matrix
+m_list$getInv_m()
+
+# Reset the orginal matrix
+m_list <- makeCacheMatrix(matrix(c(1,2,3,4), nrow=2, ncol=2))
+
+# Return original matrix
+m_list$get_m()
+
+# Return the calculated inverse matrix
+cacheSolve(m_list)
+
+# Return the cached inverse matrix
+cacheSolve(m_list)
+
+# Return the inverse matrix
+m_list$getInv_m()
+
+# Store the original matrix
+m0 <- m_list$get_m()
+
+# Set the inverse matrix to the original matrix
+m_list$setInv_m(m0)
+
+# Now the original and inverse matrices are the same (which is a problem - don't use setInv_m outside of 'cacheSolve')
+m_list$get_m()
+m_list$getInv_m()
+
+# Reset the orginal matrix to a 4 by 4 and calculate its inverse
+m_list <- makeCacheMatrix(matrix(c(9,1,3,6,13,11,7,0,5,7,4,7,2,6,1,10), nrow=4, ncol=4))
+cacheSolve(m_list)
